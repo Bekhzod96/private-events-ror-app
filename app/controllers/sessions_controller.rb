33cfa2ecd3ab1885707fe841719cfabbc00b1ada
beka <mr.begzodjon96@mail.ru>
2020-06-09 19:01:13 +0300

    Log out created

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,6 +15,11 @@ class SessionsController < ApplicationController
 			render 'new'
 		end
 	end
-
+	
+	def destroy
+		session[:user_id] = nil
+		flash[:success] = "You have logged out"
+		redirect_to root_path
+	end
 
 end
