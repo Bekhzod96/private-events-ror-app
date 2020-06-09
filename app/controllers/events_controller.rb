4676cbef05017822069b2bbfdc6bb98999fca9d0
beka <mr.begzodjon96@mail.ru>
2020-06-10 00:33:32 +0300

    Event create function implemented

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,10 +6,25 @@ class EventsController < ApplicationController
 	end
 
 	def create
-		
+
+		event = current_user.events.build(event_params)
+
+		if event.save 
+			flash[:notice] = "Event created successfully!"
+			redirect_to event_path(current_user)
+		else
+			render 'new'
+		end
 	end
 
 	def show
 		
 	end
+
+	protected
+
+	def event_params
+		params.require(:event).permit(:title, :date, :description)
+	end
+
 end
