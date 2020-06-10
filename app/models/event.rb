42c8f8a2dd80f899f38c27c327d38ac2a8f4d074
Javier Botero <javierbotero10271@gmail.com>
2020-06-10 11:57:02 -0500

    Add comments that could improve the code relations in the models

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,8 @@ class Event < ApplicationRecord
   belongs_to :user, :foreign_key => 'creator'
   has_many :user_attendees
   # has_many :users, through: :user_attendees
+
+  # has_many :user_attendees, foreign_key: :event_attendee_id
+  # has_many :atendees, through: :user_attendees, source: :event_creator
+  # belongs_to :creator, class_name: "User"
 end
